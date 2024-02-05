4d97507c7b36aa4eef1b2506a60610848dfb4f84
tcocco20 <theo@coccoscoding.dev>
2024-02-04 19:54:41 -0800

    made html and started ts

--- customVideoPlayer/custom-video-player/script.ts
+++ customVideoPlayer/custom-video-player/script.ts
@@ -1,34 +1,44 @@
-const video = document.getElementById("video");
-const playButton = document.getElementById("play");
-const stopButton = document.getElementById("stop");
-const progress = document.getElementById("progress");
-const timestamp = document.getElementById("timestamp");
+const video = document.getElementById("video") as HTMLVideoElement;
+const playButton = document.getElementById("play") as HTMLButtonElement;
+const stopButton = document.getElementById("stop") as HTMLButtonElement;
+const progress = document.getElementById("progress") as HTMLInputElement;
+const timestamp = document.getElementById("timestamp") as HTMLSpanElement;
+
 // Play & pause video
-const toggleVideoStatus = function () {
+const toggleVideoStatus = (): void => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
 };
-const updatePlayIcon = function () {
+
+const updatePlayIcon = (): void => {
   if (video.paused) {
     playButton.innerHTML = '<i class="fa fa-play fa-2x"></i>';
   } else {
     playButton.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 };
+
 // Update progress & timestamp
-const updateProgress = function () {};
+const updateProgress = (): void => {};
+
 // Set video time to progress
-const setVideoProgress = function () {};
+const setVideoProgress = (): void => {};
+
 // Stop video
-const stopVideo = function () {};
+const stopVideo = (): void => {};
+
 // Event listeners
+
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
 video.addEventListener("timeupdate", updateProgress);
+
 playButton.addEventListener("click", toggleVideoStatus);
+
 stopButton.addEventListener("click", stopVideo);
+
 progress.addEventListener("change", setVideoProgress);
