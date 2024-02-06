98ee5c9d023f99b1d29e99b55afbbd7be8fe2ee1
tcocco20 <theo@coccoscoding.dev>
2024-02-06 13:12:31 -0800

    Finished project

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -1,2 +1,40 @@
-var testStr = "Hello, World!";
-console.log(testStr);
+var currencyEl_one = document.getElementById("currency-one");
+var amountEl_one = document.getElementById("amount-one");
+var currencyEl_two = document.getElementById("currency-two");
+var amountEl_two = document.getElementById("amount-two");
+var rateEl = document.getElementById("rate");
+var swap = document.getElementById("swap");
+// Fetch exchange rates and update the DOM
+function calculate() {
+    if (currencyEl_one && currencyEl_two) {
+        var currency_one_1 = currencyEl_one.value;
+        var currency_two_1 = currencyEl_two.value;
+        axios
+            .get("https://api.exchangerate-api.com/v4/latest/".concat(currency_one_1))
+            .then(function (res) {
+            if (amountEl_one && amountEl_two && rateEl) {
+                var rate = res.data.rates[currency_two_1];
+                rateEl.innerText = "1 ".concat(currency_one_1, " = ").concat(rate, " ").concat(currency_two_1);
+                amountEl_two.value = (+amountEl_one.value * rate).toFixed(2);
+            }
+        });
+    }
+}
+// Event listeners
+if (currencyEl_one &&
+    amountEl_one &&
+    currencyEl_two &&
+    amountEl_two &&
+    rateEl &&
+    swap) {
+    currencyEl_one.addEventListener("change", calculate);
+    amountEl_one.addEventListener("input", calculate);
+    currencyEl_two.addEventListener("change", calculate);
+    amountEl_two.addEventListener("input", calculate);
+    swap.addEventListener("click", function () {
+        var temp = currencyEl_one.value;
+        currencyEl_one.value = currencyEl_two.value;
+        currencyEl_two.value = temp;
+        calculate();
+    });
+}
--- exchange-rate/script.ts
+++ exchange-rate/script.ts
@@ -1,1 +1,60 @@
-import axios from 'axios';
+declare var axios: any;
+
+const currencyEl_one = document.getElementById(
+  "currency-one"
+) as HTMLSelectElement | null;
+const amountEl_one = document.getElementById(
+  "amount-one"
+) as HTMLInputElement | null;
+
+const currencyEl_two = document.getElementById(
+  "currency-two"
+) as HTMLSelectElement | null;
+const amountEl_two = document.getElementById(
+  "amount-two"
+) as HTMLInputElement | null;
+
+const rateEl = document.getElementById("rate") as HTMLDivElement | null;
+const swap = document.getElementById("swap") as HTMLButtonElement | null;
+
+// Fetch exchange rates and update the DOM
+function calculate(): void {
+  if (currencyEl_one && currencyEl_two) {
+    const currency_one: string = currencyEl_one.value;
+    const currency_two: string = currencyEl_two.value;
+
+    axios
+      .get(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+      .then((res: any) => {
+        if (amountEl_one && amountEl_two && rateEl) {
+          const rate: number = res.data.rates[currency_two];
+
+          rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+          amountEl_two.value = (+amountEl_one.value * rate).toFixed(2);
+        }
+      });
+  }
+}
+
+// Event listeners
+if (
+  currencyEl_one &&
+  amountEl_one &&
+  currencyEl_two &&
+  amountEl_two &&
+  rateEl &&
+  swap
+) {
+  currencyEl_one.addEventListener("change", calculate);
+  amountEl_one.addEventListener("input", calculate);
+  currencyEl_two.addEventListener("change", calculate);
+  amountEl_two.addEventListener("input", calculate);
+
+  swap.addEventListener("click", () => {
+    const temp: string = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+  });
+}
